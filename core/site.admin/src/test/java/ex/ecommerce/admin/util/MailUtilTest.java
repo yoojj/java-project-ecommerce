d22534dc2bc6f57249543e368b5816d82c6da52c
yjj <yoojj.xyz@gmail.com>
2020-02-21 07:59:35 +0900

    MailUtilTest

---
+++ core/site.admin/src/test/java/ex/ecommerce/admin/util/MailUtilTest.java
@@ -0,0 +1,40 @@
+package ex.ecommerce.admin.util;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.context.web.WebAppConfiguration;
+
+import ex.ecommerce.common.util.MailUtil;
+import ex.ecommerce.common.vo.Mail;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(locations = {"classpath*:config/root-context.xml", 
+		"classpath*:config/app/*.xml"})
+@WebAppConfiguration
+public class MailUtilTest {
+	
+	@Test
+	public void test() {
+		
+		String html = "<p style='color:red'>test</p>";
+		
+		final Mail mail = new Mail();
+		
+		mail.setToMail(""); 
+		
+		mail.setGroupMail(new String[] {"", ""});
+		mail.setGroupMailHide(new String[] {""});
+
+		mail.setMailSubject("mail subject");
+		mail.setMailHTMLContent(html);
+		
+		boolean result = MailUtil.sendMail(mail);
+		
+		System.out.println(result);
+		
+	}
+	
+}
