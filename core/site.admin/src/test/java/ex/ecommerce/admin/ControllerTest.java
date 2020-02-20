b3c6b82cad6e21409a734380b1a2d288961678a9
yjj <yoojj.xyz@gmail.com>
2020-02-21 07:03:52 +0900

    ControllerTest

---
+++ core/site.admin/src/test/java/ex/ecommerce/admin/ControllerTest.java
@@ -0,0 +1,43 @@
+package ex.ecommerce.admin;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.context.web.WebAppConfiguration;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+
+import ex.ecommerce.admin.web.AdminLoginController;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(locations = {"classpath:config/root-context.xml", 
+		"classpath:config/app/servlet-context.xml"})
+@WebAppConfiguration
+public class ControllerTest {
+
+	@Autowired
+	private AdminLoginController controller;
+	
+	private MockMvc mockMvc;
+	    
+	@Before
+	public void setup() {
+		
+		this.mockMvc = MockMvcBuilders.standaloneSetup(controller).build();
+		
+	}
+
+	@Test
+	public void login() throws Exception {
+		
+		this.mockMvc.perform(get("/")).andExpect(status().isOk());
+		
+	}
+	
+}
