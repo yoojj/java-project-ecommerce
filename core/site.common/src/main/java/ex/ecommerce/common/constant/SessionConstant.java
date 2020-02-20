bd32b1a702e9477ddff5209dfb715ea2ff08549e
yjj <yoojj.xyz@gmail.com>
2020-02-20 22:29:13 +0900

    SessionConstant

---
+++ core/site.common/src/main/java/ex/ecommerce/common/constant/SessionConstant.java
@@ -0,0 +1,8 @@
+package ex.ecommerce.common.constant;
+
+public class SessionConstant {
+
+	public static final String ADMIN_KEY = "sessionAdmin";
+	public static final String USER_KEY = "sessionUser";
+	
+}
