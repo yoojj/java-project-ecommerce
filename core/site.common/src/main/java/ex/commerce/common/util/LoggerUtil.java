3a4c58fce25de54a2b1fff9f7e39bc2b343c01a8
yjj <yoojj.xyz@gmail.com>
2020-02-20 22:16:14 +0900

    LoggerUtil

---
+++ core/site.common/src/main/java/ex/commerce/common/util/LoggerUtil.java
@@ -0,0 +1,12 @@
+package ex.commerce.common.util;
+
+import java.lang.invoke.MethodHandles;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public final class LoggerUtil {
+
+	public static Logger log = LoggerFactory.getLogger(MethodHandles.lookup().lookupClass());
+	
+}
