d3ed1566ca975e9cdb335544f4a0d35f7a0c9054
yjj <yoojj.xyz@gmail.com>
2020-02-24 00:26:14 +0900

    BCryptCheckUtil

---
+++ core/site.common/src/main/java/ex/ecommerce/common/util/BCryptCheckUtil.java
@@ -0,0 +1,18 @@
+package ex.ecommerce.common.util;
+
+import at.favre.lib.crypto.bcrypt.BCrypt;
+
+public class BCryptCheckUtil {
+
+	public static boolean check(String pwd, String dbPwd) {
+		
+		final BCrypt.Result result = BCrypt.verifyer().verify(pwd.toCharArray(), dbPwd);
+		
+		if( result.verified == true ) {
+			return true;
+		}
+		
+		return false;
+	}
+	
+}
