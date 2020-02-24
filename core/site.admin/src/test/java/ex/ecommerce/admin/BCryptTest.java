2be2ab2bc9f785e0b73de9d32aded0ab4833fd76
yjj <yoojj.xyz@gmail.com>
2020-02-25 01:30:11 +0900

    BCryptTest

---
+++ core/site.admin/src/test/java/ex/ecommerce/admin/BCryptTest.java
@@ -0,0 +1,31 @@
+package ex.ecommerce.admin;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.context.web.WebAppConfiguration;
+
+import at.favre.lib.crypto.bcrypt.BCrypt;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(locations = {"classpath:*.xml"})
+@WebAppConfiguration
+public class BCryptTest {
+
+	@Test
+	public void test() {
+		
+		String password = "0000";
+
+		String bcryptHashString = BCrypt.withDefaults().hashToString(12, password.toCharArray());
+		
+		BCrypt.Result result = BCrypt.verifyer().verify(password.toCharArray(), bcryptHashString);
+		
+		System.out.println("비밀번호 : " + password);
+		System.out.println("암호화 : " + bcryptHashString );
+		System.out.println("비교 : " + result.verified  );
+
+	}
+	
+}
