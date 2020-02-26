47432f974ec04e0ee257e0c984d7d02f7f3aa780
yjj <yoojj.xyz@gmail.com>
2020-02-26 23:40:53 +0900

    CRUDTest

--- core/site.admin/src/test/java/ex/ecommerce/admin/db/CRUDTest.java
+++ core/site.admin/src/test/java/ex/ecommerce/admin/db/CRUDTest.java
@@ -2,10 +2,15 @@ package ex.ecommerce.admin.db;
 
 import java.sql.Connection;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 import org.junit.AfterClass;
 import org.junit.BeforeClass;
+import org.junit.Ignore;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.test.context.ContextConfiguration;
@@ -25,6 +30,7 @@ public class CRUDTest {
 	private static HikariDataSource ds;
 	private static Connection connection;
 	private static PreparedStatement ps;
+	private ResultSet rs;
 	
 	@BeforeClass
 	public static void connection() throws SQLException {
@@ -50,7 +56,7 @@ public class CRUDTest {
 		
 	}
 	
-	@Test
+	@Test @Ignore
 	public void CreateTableTest() throws SQLException {
 		
 		StringBuilder sb = new StringBuilder();
@@ -64,5 +70,100 @@ public class CRUDTest {
 		ps.execute();
 	
 	}
+	
+	@Test @Ignore
+	public void insertTest() throws SQLException {
 
+		try {
+			
+			String sql = "INSERT INTO test VALUES (?, ?)";
+			
+			ps = connection.prepareStatement(sql);
+			
+			ps.setString(1, "id-test");
+			ps.setString(2, "pwd-test");
+			
+			ps.addBatch();
+			ps.clearParameters();
+
+		} finally {
+			ps.executeBatch();
+			ps.clearBatch();
+		}
+	}
+
+	@Test @Ignore
+	public void readTest() throws SQLException {
+
+		try {
+			
+			String sql = "SELECT * FROM test";
+			
+			ps = connection.prepareStatement(sql);
+			
+			rs = ps.executeQuery();
+			
+			List<String[]> list = new ArrayList<>();
+
+			while(rs.next()) {
+				
+				String id = rs.getString("id");
+				String pwd = rs.getString("pwd");
+				
+				list.add(new String[] {id, pwd});
+				
+			}
+
+			for(String[] value : list) 
+				System.out.println(Arrays.toString(value));
+			
+		} finally {
+			ps.clearParameters();
+			rs.close();
+		}
+
+	}
+	
+	@Test @Ignore
+	public void modifyTest() throws SQLException {
+
+		try {
+			
+			String sql = "UPDATE test SET pwd=? WHERE id=?";
+			
+			ps = connection.prepareStatement(sql);
+			
+			ps.setString(1, "pwd-000000");
+			ps.setString(2, "id-test");
+
+			int result = ps.executeUpdate();
+			System.out.println(result);
+
+		} finally {
+			ps.clearParameters();
+		}
+
+	}
+	
+	@Test
+	public void deleteTest() throws SQLException {
+
+		try {
+			
+			String sql = "DELETE test WHERE id=? AND pwd=?";
+			
+			ps = connection.prepareStatement(sql);
+			
+			ps.setString(1, "id-test");
+			ps.setString(2, "pwd-000000");
+
+			int result = ps.executeUpdate();
+			System.out.println(result);
+
+		} finally {
+			ps.clearParameters();
+		}
+
+	}
+	
 }
