987e609f39acc7a3ff69cc7a12fc930c9a34eebf
yjj <yoojj.xyz@gmail.com>
2020-03-24 23:43:51 +0900

    AdminDAOTest

--- core/site.admin/src/test/java/ex/ecommerce/admin/db/AdminDAOTest.java
+++ core/site.admin/src/test/java/ex/ecommerce/admin/db/AdminDAOTest.java
@@ -1,5 +1,10 @@
 package ex.ecommerce.admin.db;
 
+import static org.junit.Assert.assertEquals;
+
+import java.sql.SQLException;
+
+import org.junit.Ignore;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -7,20 +12,44 @@ import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.test.context.web.WebAppConfiguration;
 
-import ex.ecommerce.admin.module.AdminDAOImpl;
+import ex.ecommerce.admin.module.member.AdminRegDTO;
+import ex.ecommerce.admin.module.member.impl.AdminDAOImpl;
 
 @RunWith(SpringJUnit4ClassRunner.class)
-@ContextConfiguration(locations = {"classpath*:config/root-context.xml", 
-		"classpath*:config/app/*.xml"})
+@ContextConfiguration(locations = {"classpath*:config/common-context.xml", 
+		"classpath*:config/db-context.xml", "classpath*:config/mail-context.xml",
+		"classpath*:config/app/servlet-context.xml"})
 @WebAppConfiguration
 public class AdminDAOTest {
 
 	@Autowired
 	private AdminDAOImpl adminDAO;
 	
-	@Test
-	public void test() {
+	@Test @Ignore
+	public void insertTeset() throws SQLException {
 		
+		final AdminRegDTO adminRegDTO = new AdminRegDTO();
+		
+		adminRegDTO.setId("test-id");
+		adminRegDTO.setPwd("test-pwd");
+		adminRegDTO.setLevel("1");
+		adminRegDTO.setRegAdmin("test");
+		adminRegDTO.setRegMsg("test insert");
+		adminRegDTO.setState(true);
+
+		int result = adminDAO.insert(adminRegDTO);
+		
+		assertEquals(result, 1);
+		
+	}
+	
+	@Test @Ignore
+	public void deleteTest() throws SQLException {
+
+		int result = adminDAO.realDelete("test-id");
+
+		assertEquals(result, 1);
+
 	}
 	
 }
