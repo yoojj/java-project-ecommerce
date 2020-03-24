4366a5539066cca901dfd367519f89106e2b1285
yjj <yoojj.xyz@gmail.com>
2020-03-25 06:02:23 +0900

    UserAddressList -- 임시

---
+++ core/site.common/src/main/java/ex/ecommerce/common/vo/site/UserAddressList.java
@@ -0,0 +1,17 @@
+package ex.ecommerce.common.vo.site;
+import java.io.Serializable;
+
+@SuppressWarnings("serial")
+public class UserAddressList implements Serializable {
+
+	private int uano;
+	private String id;
+	private String addressName;
+	private String addressTel;
+	private String zipCode;	
+	private String addressBase;
+	private String addressDetail;
+	private String addressRegDate;
+	private boolean isActive;
+
+}	
