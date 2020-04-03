1bcd1b2e6de349f67096b0a3d6aea9731b253704
yjj <yoojj.xyz@gmail.com>
2020-04-03 11:24:32 +0900

    PwdEncryptionFilter

---
+++ core/site.common/src/main/java/ex/ecommerce/common/filter/PwdEncryptionFilter.java
@@ -0,0 +1,37 @@
+package ex.ecommerce.common.filter;
+
+import java.io.IOException;
+
+import javax.servlet.Filter;
+import javax.servlet.FilterChain;
+import javax.servlet.FilterConfig;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.annotation.WebFilter;
+import javax.servlet.http.HttpServletRequest;
+
+import ex.ecommerce.common.module.pwdencryption.PwdEncryptWrapper;
+
+@WebFilter
+public class PwdEncryptionFilter implements Filter {
+
+	@Override
+	public void destroy() {
+	}
+
+	@Override
+	public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain)
+			throws IOException, ServletException {
+
+		PwdEncryptWrapper wrapper = new PwdEncryptWrapper((HttpServletRequest) request);
+
+		chain.doFilter(wrapper, response);
+		
+	}
+
+	@Override
+	public void init(FilterConfig config) throws ServletException {
+	}
+
+}
