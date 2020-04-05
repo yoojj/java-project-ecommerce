a8918adc2eea95e2804fa0bcab8cb8e9c85ce7f7
yjj <yoojj.xyz@gmail.com>
2020-04-06 04:50:16 +0900

    BCryptTest

--- core/site.admin/src/test/java/ex/ecommerce/admin/BCryptTest.java
+++ core/site.admin/src/test/java/ex/ecommerce/admin/BCryptTest.java
@@ -1,23 +1,15 @@
 package ex.ecommerce.admin;
 
 import org.junit.Test;
-import org.junit.runner.RunWith;
-import org.springframework.test.context.ContextConfiguration;
-import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
-import org.springframework.test.context.web.WebAppConfiguration;
 
 import at.favre.lib.crypto.bcrypt.BCrypt;
 
-@RunWith(SpringJUnit4ClassRunner.class)
-@ContextConfiguration(locations = {"classpath*:config/common-context.xml", 
-		"classpath*:config/db-context.xml", "classpath*:config/mail-context.xml"})
-@WebAppConfiguration
 public class BCryptTest {
 
 	@Test
 	public void test() {
-		
-		String password = "0000";
+
+		String password = "test";
 
 		String bcryptHashString = BCrypt.withDefaults().hashToString(12, password.toCharArray());
 		
