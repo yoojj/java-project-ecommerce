1a6fe34519f3c3fb135ef0a858fe880ffaa43d63
yjj <yoojj.xyz@gmail.com>
2020-04-06 14:10:48 +0900

    SessionScopeUtil -- response 추가

--- core/site.common/src/main/java/ex/ecommerce/common/util/SessionScopeUtil.java
+++ core/site.common/src/main/java/ex/ecommerce/common/util/SessionScopeUtil.java
@@ -1,6 +1,7 @@
 package ex.ecommerce.common.util;
 
 import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 
 import org.springframework.web.context.request.RequestContextHolder;
 import org.springframework.web.context.request.ServletRequestAttributes;
@@ -9,6 +10,10 @@ public final class SessionScopeUtil {
 
 	public static HttpServletRequest getRequest() { 
 		return ((ServletRequestAttributes)RequestContextHolder.currentRequestAttributes()).getRequest();
+	}
+	
+	public static HttpServletResponse getResponse() { 
+		return ((ServletRequestAttributes)RequestContextHolder.currentRequestAttributes()).getResponse();
 	}
 	
 }
