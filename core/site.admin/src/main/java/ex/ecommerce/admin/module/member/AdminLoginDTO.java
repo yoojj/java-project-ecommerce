36768e912001125ecd5ff1fe7c25a2a034a4da13
yjj <yoojj.xyz@gmail.com>
2020-04-07 13:52:12 +0900

    AdminLoginDTO -- 필드 추가

--- core/site.admin/src/main/java/ex/ecommerce/admin/module/member/AdminLoginDTO.java
+++ core/site.admin/src/main/java/ex/ecommerce/admin/module/member/AdminLoginDTO.java
@@ -12,35 +12,48 @@ public class AdminLoginDTO implements Serializable {
 	
 	@Size(min=ValidConstant.ID_MIN_NUM, message="아이디는 " + ValidConstant.ID_MIN_NUM + "글자 이상입니다.")
 	@Pattern(regexp=ValidConstant.REG_ENG_NUM, message="아이디는 영문 및 숫자만 입력 가능합니다.")
-	private String adminId;
+	private String id;
 	
 	@Size(min=ValidConstant.PWD_MIN_NUM, message="비밀번호는 " + ValidConstant.PWD_MIN_NUM + "글자 이상입니다.")
-	private String adminPwd;
+	private String pwd;
 
-	public String getAdminId() {
-		return adminId;
+	private String clientIp;
+	
+	//private String sessionId;
+	
+	public String getId() {
+		return id;
 	}
 
-	public void setAdminId(String adminId) {
-		this.adminId = adminId;
+	public void setId(String id) {
+		this.id = id;
 	}
 
-	public String getAdminPwd() {
-		return adminPwd;
+	public String getPwd() {
+		return pwd;
 	}
 
-	public void setAdminPwd(String adminPwd) {
-		this.adminPwd = adminPwd;
+	public void setPwd(String pwd) {
+		this.pwd = pwd;
 	}
 
+	public String getClientIp() {
+		return clientIp;
+	}
+
+	public void setClientIp(String clientIp) {
+		this.clientIp = clientIp;
+	}
+	
 	public String toSting(){
 		
 		StringBuffer sb = new StringBuffer();
 		
 		sb.append("\n");
 		sb.append(this.getClass().getName()).append("\n");
-		sb.append("아이디    [ " + adminId + " ] \n");
-		sb.append("비밀번호  [ " + adminPwd + " ] \n");
+		sb.append("아이디    [ " + id + " ] \n");
+		sb.append("비밀번호  [ " + pwd + " ] \n");
+		sb.append("아이피    [ " + clientIp + " ] \n");
 		
 		return sb.toString();
 	}
