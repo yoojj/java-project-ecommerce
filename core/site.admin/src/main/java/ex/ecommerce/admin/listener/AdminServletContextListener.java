80245698aab954e376c789b291cccb93fc4a52a3
yjj <yoojj.xyz@gmail.com>
2020-08-04 23:00:09 +0900

    AdminServletContextListener -- 주석 추가

--- core/site.admin/src/main/java/ex/ecommerce/admin/listener/AdminServletContextListener.java
+++ core/site.admin/src/main/java/ex/ecommerce/admin/listener/AdminServletContextListener.java
@@ -8,17 +8,21 @@ import ex.ecommerce.common.constant.AdminLevelEnum;
 
 public class AdminServletContextListener implements ServletContextListener {
 
+	/**
+	 * 웹 애플리케이션 시작시 관리자 등급 데이터 전송  
+	 */
+	
 	@Override
-	public void contextInitialized(ServletContextEvent event) {
+	public void contextInitialized(ServletContextEvent e) {
 
-		final ServletContext context = event.getServletContext();
+		final ServletContext context = e.getServletContext();
 		
 		context.setAttribute("adminLevelEnum", AdminLevelEnum.values());
 
 	}
 	
 	@Override
-	public void contextDestroyed(ServletContextEvent event) {
+	public void contextDestroyed(ServletContextEvent e) {
 	}
 
 }
