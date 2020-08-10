76237677c6766ef7b7e95f1687323d4d793892f8
yjj <yoojj.xyz@gmail.com>
2020-08-11 08:11:07 +0900

    UserSessionDTO

---
+++ core/site/src/main/java/ex/ecommerce/site/module/user/UserSessionDTO.java
@@ -0,0 +1,69 @@
+package ex.ecommerce.site.module.user;
+
+import java.io.Serializable;
+
+@SuppressWarnings("serial") 
+public class UserSessionDTO implements Serializable {
+
+	private String id;
+	private int level;
+	private String clientIp;
+	private String sessionId;
+	private boolean state;
+
+	public String getId() {
+		return id;
+	}
+
+	public void setId(String id) {
+		this.id = id;
+	}
+
+	public int getLevel() {
+		return level;
+	}
+
+	public void setLevel(int level) {
+		this.level = level;
+	}
+
+	public String getClientIp() {
+		return clientIp;
+	}
+
+	public void setClientIp(String clientIp) {
+		this.clientIp = clientIp;
+	}
+
+	public String getSessionId() {
+		return sessionId;
+	}
+
+	public void setSessionId(String sessionId) {
+		this.sessionId = sessionId;
+	}
+
+	public boolean isState() {
+		return state;
+	}
+
+	public void setState(boolean state) {
+		this.state = state;
+	}
+	
+	public String toSting(){
+		
+		StringBuilder sb = new StringBuilder();
+		
+		sb.append("\n");
+		sb.append(this.getClass().getName()).append("\n");
+		sb.append("아이디       [ " + id + " ] \n");
+		sb.append("등급         [ " + level + " ] \n");
+		sb.append("아이피       [ " + clientIp + " ] \n");
+		sb.append("세션 아이디  [ " + sessionId + " ] \n");
+		sb.append("사용 여부    [ " + state + " ] \n");
+		
+		return sb.toString();
+	}
+	
+}
