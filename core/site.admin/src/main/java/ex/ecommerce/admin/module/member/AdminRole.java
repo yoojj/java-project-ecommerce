36517ffba5038e03b88ac9e0401e56503809fa51
yjj <yoojj.xyz@gmail.com>
2020-11-02 23:36:01 +0900

    AdminRole

---
+++ core/site.admin/src/main/java/ex/ecommerce/admin/module/member/AdminRole.java
@@ -0,0 +1,20 @@
+package ex.ecommerce.admin.module.member;
+
+public class AdminRole {
+
+	/** 코드 번호 */
+	/** 코드 설명 */
+	
+	/** 그룹 명 */
+	/** 그룹 번호 */
+	
+	/** 그룹 코드 */
+	/** 하위 코드 */
+	
+	/** 등록자 */
+	/** 등록일 */
+	
+	/** 수정자 */
+	/** 수정일 */
+	
+}
